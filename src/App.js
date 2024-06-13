2294406152a879cf8e9eac422d1e8b1f44adb673
manormasharma05 <manorma@hackerearth.com>
2024-06-13 12:58:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+function App() {
+  return (
+    <div className="App font-semibold">
+      <Header/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar-items mx-3 py-5'>
+        <ul className='border-b pb-3'>
+            <li>Home</li>
+            <li>Shorts</li>
+            <li>Subscription</li>
+        </ul>
+        <p>You </p>
+        <ul className='border-b pb-3'>
+            <li>History</li>
+            <li>Playlist</li>
+            <li>Watch Later</li>
+            <li>Liked Videos</li>
+        </ul>
+        <p>Subscription </p>
+        <ul className='border-b pb-3'>
+            <li>Zee Bussiness</li>
+            <li>Technical Baba</li>
+            <li>Aaj Tak Live</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Sidebar
